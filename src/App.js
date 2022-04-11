3df29568916db8abf42c1d8d0f4de1aa6606ad93
Magali Fabri <magali_90@live.nl>
2022-04-11 11:21:44 +0200

    Display todolist with static todos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import React, {useState} from "react";
+import TodoList from "./TodoList";
+
 function App() {
-  return null;
+    const [todos, setTodos] = useState([
+        {
+            id: 1,
+            name: 'Todo 1',
+            complete: false,
+        },
+        {
+            id: 2,
+            name: 'Todo 2',
+            complete: false,
+        }
+    ]);
+
+    return (
+        <>
+            <TodoList todos={todos}/>
+            <input type="text"/>
+            <button>Add Todo</button>
+            <button>Clear completed</button>
+            <div>0 left Todos</div>
+        </>
+    )
 }
 
 export default App;
