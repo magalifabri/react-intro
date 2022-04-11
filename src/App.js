3991257caef68cb07744c9dbe5e7c28ebe5f79bc
Magali Fabri <magali_90@live.nl>
2022-04-11 14:55:05 +0200

    Store states in local storage & add button to clear todos

--- src/App.js
+++ src/App.js
@@ -1,38 +1,66 @@
-// useState Hook lets us keep local state in a function component
-import React, {useState, useRef} from "react";
+import React, {useState, useRef, useEffect} from "react";
 import TodoList from "./TodoList";
+const LOCAL_STORAGE_TODOS_KEY = 'todos';
+const LOCAL_STORAGE_ID_COUNTER_KEY = 'idCounter';
+
 
 function App() {
     const todoNameRef = useRef();
-    // useState() takes one argument: the initial state
-    // useState() returns two variables: the current state and a function to update the state
     const [todos, setTodos] = useState([]);
+    const [idCounter, setIdCounter] = useState(0);
 
-    const [idCounter, setIdCounter] = useState(1);
+
+    // load states from local storage
+    useEffect(() => {
+        const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TODOS_KEY));
+        const storedIdCounter = Number(localStorage.getItem(LOCAL_STORAGE_ID_COUNTER_KEY));
+
+        if (storedTodos && storedTodos.length > 0) {
+            setTodos(storedTodos);
+        }
+
+        if (storedIdCounter) {
+            setIdCounter(storedIdCounter);
+        }
+    }, []);
+
+
+    // save states in local storage
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_TODOS_KEY, JSON.stringify(todos))
+        localStorage.setItem(LOCAL_STORAGE_ID_COUNTER_KEY, String(idCounter));
+    }, [todos, idCounter]);
+
 
     const getTodosWithNewTodoAdded = (newTodoName, prevTodos) => {
-        const id = idCounter;
         setIdCounter(idCounter + 1);
 
         return [...prevTodos, {
-            id: id,
+            id: idCounter,
             name: newTodoName,
             complete: false,
         }];
     }
 
-    const handleAddTodo = (event) => {
-        const name = todoNameRef.current.value;
-        // console.log(name);
 
-        if (name === '') {
+    const handleAddTodo = (event) => {
+        const newTodoName = todoNameRef.current.value;
+
+        if (newTodoName === '') {
             return;
         }
 
-        setTodos(prevTodos => getTodosWithNewTodoAdded(name, prevTodos));
+        setTodos(prevTodos => getTodosWithNewTodoAdded(newTodoName, prevTodos));
 
+        // empty input field
         todoNameRef.current.value = '';
     }
+
+
+    const clearAllTodos = () => {
+        localStorage.clear();
+    }
+
 
     return (
         <>
@@ -40,6 +68,7 @@ function App() {
             <input ref={todoNameRef} type="text"/>
             <button onClick={handleAddTodo}>Add Todo</button>
             <button>Clear completed</button>
+            <button onClick={clearAllTodos}>Clear all todos</button>
             <div>0 left Todos</div>
         </>
     )
