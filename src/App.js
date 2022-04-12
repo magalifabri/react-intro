cf0da974159c4ed28848c4e62961fb1a0814e7ea
Magali Fabri <magali_90@live.nl>
2022-04-12 11:00:31 +0200

    Add navbar component (WIP)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import TodoList from "./TodoList";
+import Nav from "./Nav";
 
 
 const App = () => {
 
     return (
         <Router>
+            <Nav />
+
             <Routes>
                 <Route path="/" element={<TodoList/>} />
             </Routes>
---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div className="nav">
+            <ul>
+                <li>nav item 1</li>
+                <li>nav item 2</li>
+                <li>nav item 3</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Nav;
