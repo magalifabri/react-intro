a7c489ba39e51e152f2dac6ced4780be305aa304
Magali Fabri <magali_90@live.nl>
2022-04-13 11:31:43 +0200

    Display todo for day on day hover

--- src/CalendarPage.js
+++ src/CalendarPage.js
@@ -2,14 +2,39 @@ import React from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import {differenceInCalendarDays, parseISO} from 'date-fns';
+import './style.css';
 
 
 const CalendarPage = ({todos}) => {
-    function tileContent({date, view}) {
-        // Add to-do indication in month view only
+
+    // add pop-up div to days with associated todos for styling (month-view only)
+    function tileContent(e) {
+        const date = e.date;
+        const view = e.view;
+
+        if (view === 'month') {
+            const todosOnThisDay = todos.filter(todo => differenceInCalendarDays(parseISO(todo.date), date) === 0);
+
+            if (todosOnThisDay.length) {
+                let i = 0;
+
+                return (
+                    <div className="todo-popup">
+                        <ul>
+                            {todosOnThisDay.map(todo => <li key={i++}>{todo.name}</li>)}
+                        </ul>
+                    </div>
+                )
+            }
+        }
+    }
+
+
+    // add className to days with associated todos for styling (month-view only)
+    function tileClassName({date, view}) {
         if (view === 'month') {
             if (todos.find(todo => differenceInCalendarDays(parseISO(todo.date), date) === 0)) {
-                return ' todo';
+                return 'has-todo'
             }
         }
     }
@@ -21,6 +46,7 @@ const CalendarPage = ({todos}) => {
 
             <Calendar
                 tileContent={tileContent}
+                tileClassName={tileClassName}
             />
         </div>
     );
