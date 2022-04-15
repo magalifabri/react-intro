ec8f7a2cf220a10cbc0e9bbf96e9dec24ac6735c
Magali Fabri <magali_90@live.nl>
2022-04-15 08:46:44 +0200

    BEMify Nav scss

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,13 +2,39 @@ import React from 'react';
 import {NavLink} from "react-router-dom";
 import '../styles/NavStyle.scss'
 
+
 const Nav = () => {
+
+    const getItemClassNames = (isActive) => {
+        if (isActive) {
+            return "nav__link nav__link--active";
+        } else {
+            return "nav__link";
+        }
+    };
+
+
     return (
-        <nav>
-            <ul>
-                <li><NavLink to="/" className={({isActive}) => (isActive ? "active" : 'none')}>to-do list</NavLink></li>
-                <li><NavLink to="/calendar" className={({isActive}) => (isActive ? "active" : 'none')}>calendar</NavLink></li>
-                <li><NavLink to="/notes" className={({isActive}) => (isActive ? "active" : 'none')}>notes</NavLink></li>
+        <nav className="nav">
+            <ul className="nav__list">
+                <li className="nav__item">
+                    <NavLink to="/"
+                             className={({isActive}) => getItemClassNames(isActive)}>
+                        to-do
+                    </NavLink>
+                </li>
+                <li className="nav__item">
+                    <NavLink to="/calendar"
+                             className={({isActive}) => getItemClassNames(isActive)}>
+                        calendar
+                    </NavLink>
+                </li>
+                <li className="nav__item">
+                    <NavLink to="/notes"
+                             className={({isActive}) => getItemClassNames(isActive)}>
+                        notes
+                    </NavLink>
+                </li>
             </ul>
         </nav>
     );
