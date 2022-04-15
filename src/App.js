9a0269db93e27f07453a2f80237c0dff2bd43a22
Magali Fabri <magali_90@live.nl>
2022-04-15 15:16:10 +0200

    Add Footer and change imports order in App.js to resolve styling specificity conflicts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {useEffect, useState} from "react";
+
+// import general styling before function components
+// so that the styling that the components import
+// overwrites same-specificity general styling
+import './styles/style.scss';
+
+// function components:
 import TodoList from "./pages/TodoList";
 import Nav from "./components/Nav";
 import Notes from "./pages/Notes";
 import CalendarPage from "./pages/CalendarPage";
-import {useEffect, useState} from "react";
-import './styles/style.scss';
+import Footer from "./components/Footer";
 
 export const LOCAL_STORAGE_TODOS_KEY = 'todos';
 
@@ -33,13 +40,21 @@ const App = () => {
             <Nav/>
 
             <Routes>
-                <Route path="/" element={<TodoList
-                    todos={todos}
-                    setTodos={setTodos}
-                />}/>
-                <Route path="/calendar" element={<CalendarPage todos={todos}/>}/>
+                <Route path="/" element={
+                    <TodoList
+                        todos={todos}
+                        setTodos={setTodos}
+                    />
+                }/>
+
+                <Route path="/calendar" element={
+                    <CalendarPage todos={todos}/>
+                }/>
+
                 <Route path="/notes" element={<Notes/>}/>
             </Routes>
+
+            <Footer/>
         </Router>
     )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,62 @@
+import React, {useState} from 'react';
+import '../styles/Footer.scss';
+
+
+const Footer = () => {
+    const [overlayActive, setOverlayActive] = useState(false);
+
+    const toggleOverlay = (event) => {
+        if (!event.target.classList.contains('overlay--active')
+            && !event.target.classList.contains('footer__info-button')) {
+            return;
+        }
+
+        const infoButton = document.querySelector('.footer__info-button');
+
+        if (!overlayActive) {
+            document.body.style.overflow = 'hidden';
+            infoButton.classList.toggle('footer__info-button--active');
+            infoButton.textContent = 'close';
+        } else {
+            document.body.style.overflow = 'scroll';
+            infoButton.classList.toggle('footer__info-button--active');
+            infoButton.textContent = 'site info';
+        }
+
+        setOverlayActive(!overlayActive);
+    }
+
+    return (
+        <footer className="footer">
+            <button className="footer__info-button button-style-2"
+                    onClick={toggleOverlay}
+            >
+                site info
+            </button>
+
+            <div className={overlayActive ? 'overlay--active' : 'overlay'}
+                 onClick={toggleOverlay}
+            >
+
+                <div className="overlay__window">
+                    <p className="overlay__p">
+                        This site was made as an exercise for the coding school
+                        BeCode in Belgium.
+                    </p>
+
+                    <p className="overlay__p">
+                        View source code on <a className="overlay__a"
+                                               href="https://github.com/magalifabri/react-intro/tree/main/src/styles">
+                        GitHub</a>.
+                    </p>
+
+                    <p className="overlay__p">
+                        Made by Magali Fabri.
+                    </p>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
