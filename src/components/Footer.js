d74e6a9b2f3da97cb33148f2b47cccd3da8a803f
Magali Fabri <magali_90@live.nl>
2022-04-16 13:36:19 +0200

    Update overlay:
    - Control it's displaying with conditional rendering instead of conditional class assignment + css
    - Add transition effect to (dis)appearance
    - Make first if statement in toggleOverlay() inclusive (was exclusive) for readability

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,56 +5,77 @@ import '../styles/Footer.scss';
 const Footer = () => {
     const [overlayActive, setOverlayActive] = useState(false);
 
+
     const toggleOverlay = (event) => {
-        if (!event.target.classList.contains('overlay--active')
-            && !event.target.classList.contains('footer__info-button')) {
+        // don't close the overlay when clicking on the overlay window or it's children
+        if (event.target.classList.contains('overlay__window')
+            || event.target.classList.contains('overlay__p')
+            || event.target.classList.contains('overlay__a')) {
             return;
         }
 
-        const infoButton = document.querySelector('.footer__info-button');
-
         if (!overlayActive) {
+            // prevent scrolling of page 'beneath' overlay
             document.body.style.overflow = 'hidden';
-            infoButton.classList.toggle('footer__info-button--active');
-            infoButton.textContent = 'close';
-        } else {
-            document.body.style.overflow = 'scroll';
-            infoButton.classList.toggle('footer__info-button--active');
-            infoButton.textContent = 'site info';
-        }
 
-        setOverlayActive(!overlayActive);
+            // without setTimeout the querySelector returns null
+            setTimeout(() => {
+                document.querySelector('.overlay').style.opacity = 1;
+            }, 0)
+
+            setOverlayActive(true);
+        } else {
+            document.querySelector('.overlay').style.opacity = 0;
+
+            // give overlay time to fade out before removing it from the DOM
+            setTimeout(() => {
+                document.body.style.overflow = 'scroll';
+                setOverlayActive(false);
+            }, 100)
+        }
     }
+
 
     return (
         <footer className="footer">
-            <button className="footer__info-button button-style-2"
+            {overlayActive ?
+                <button
+                    className="footer__info-button footer__info-button--active button-style-2"
                     onClick={toggleOverlay}
-            >
-                site info
-            </button>
+                >
+                    close
+                </button>
+                :
+                <button className="footer__info-button button-style-2"
+                        onClick={toggleOverlay}
+                >
+                    site info
+                </button>
+            }
 
-            <div className={overlayActive ? 'overlay--active' : 'overlay'}
-                 onClick={toggleOverlay}
-            >
+            {overlayActive &&
+                <div className="overlay"
+                     onClick={toggleOverlay}
+                >
 
-                <div className="overlay__window">
-                    <p className="overlay__p">
-                        This site was made as an exercise for the coding school
-                        BeCode in Belgium.
-                    </p>
+                    <div className="overlay__window">
+                        <p className="overlay__p">
+                            This site was made as an exercise for the coding
+                            school BeCode in Belgium.
+                        </p>
 
-                    <p className="overlay__p">
-                        View source code on <a className="overlay__a"
-                                               href="https://github.com/magalifabri/react-intro">
-                        GitHub</a>.
-                    </p>
+                        <p className="overlay__p">
+                            View source code on <a className="overlay__a"
+                                                   href="https://github.com/magalifabri/react-intro">
+                            GitHub</a>.
+                        </p>
 
-                    <p className="overlay__p">
-                        Made by Magali Fabri.
-                    </p>
+                        <p className="overlay__p">
+                            Made by Magali Fabri.
+                        </p>
+                    </div>
                 </div>
-            </div>
+            }
         </footer>
     );
 };
